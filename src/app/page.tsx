840b64c018098db1ec560876893f678f6ecf97d8
Ömer Yalhı <omer@yalhi.com>
2023-11-23 16:04:19 -0800

    initial eslint setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <h2 className={styles.title}>Torba Split Cost</h2>
-        <hr />
-        <p className={styles.text}>coming soon...</p>
-      </div>
+    <main>
+      <h1>Torba</h1>
     </main>
   );
 }
